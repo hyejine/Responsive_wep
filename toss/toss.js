3643d7482f8a09246cd1144491d3ed01527bc73d
junghyejin <hyejine1997@naver.com>
2021-11-26 11:37:37 +0900

    11/26(금) toss header 부분 이해 안감

--- toss/toss.js
+++ toss/toss.js
@@ -8,6 +8,11 @@ $(document).ready(function(){
         }
     });
 
+    // 스크롤 이동시 애니메이션 (아래->위)
+    $(".section3").scroll(function(){
+        $('.section3_con1 ').animate({'opacity':'1'},500);
+    })
+
     // $(window).scroll(function(){
     //     var scroll=$(window).scrollTop();
     //     if(scoll>=50){
@@ -16,28 +21,30 @@ $(document).ready(function(){
     //         $(".header_inner").removeClass("header_border");
     //     };
     // });
-    // $(window).scroll (function(){
-    //     $('.section3_con1 ').animate({'opacity':'1'},500);
-    //         }
-    //     ); 
-    function isElementUnderBottom(elem, triggerDiff) {
-        const { top } = elem.getBoundingClientRect();
-        const { innerHeight } = window;
-        return top > innerHeight + (triggerDiff || 0);
-        }
-
-        function handleScroll() {
-        const elems = document.querySelectorAll('.section3_con1');
-        elems.forEach(elem => {
-            if (isElementUnderBottom(elem, -20)) {
-            elem.style.opacity = "0";
-            elem.style.transform = 'translateY(200px)';
-            } else {
-            elem.style.opacity = "1";
-            elem.style.transform = 'translateY(0px)';
+    $(window).scroll (function(){
+        $('.section3_con1 ').animate({'opacity':'1'},500);
             }
-        })
-        }
+        ); 
 
-        window.addEventListener('scroll', handleScroll);
+
+    // function isElementUnderBottom(elem, triggerDiff) {
+    //     const { top } = elem.getBoundingClientRect();
+    //     const { innerHeight } = window;
+    //     return top > innerHeight + (triggerDiff || 0);
+    //     }
+
+    //     function handleScroll() {
+    //     const elems = document.querySelectorAll('.section3_con1');
+    //     elems.forEach(elem => {
+    //         if (isElementUnderBottom(elem, -20)) {
+    //         elem.style.opacity = "0";
+    //         elem.style.transform = 'translateY(200px)';
+    //         } else {
+    //         elem.style.opacity = "1";
+    //         elem.style.transform = 'translateY(0px)';
+    //         }
+    //     })
+    //     }
+
+    //     window.addEventListener('scroll', handleScroll);
 })
