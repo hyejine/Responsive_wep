f350453ab9a406399287d1ba760716a8fa059d6f
junghyejin <hyejine1997@naver.com>
2021-12-02 16:55:27 +0900

    12/2 (목) 켈로그 섹션 4 css, 제이쿼리 hover 기능

--- kellogg/kellogg.js
+++ kellogg/kellogg.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+
+    // nav menu창 
     $(".gnb_nav_ul").hide();
     $(".gnb_nav1").hover(function(){
         $(this).children("ul").slideToggle("fast");
     },
     );
+
+    $(".s4_c1_inner").hide();
+    $(".s4_c").hover(function(){
+        $(this).stop().hide();
+        $(this).stop().next().show();
+    },
+    function(){
+        $(this).stop().show();
+        $(this).stop().next().hide();
+    });
+
+
 });
