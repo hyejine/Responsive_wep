5e1a909ce0a8d120c33b611178d7e326dba43561
junghyejin <hyejine1997@naver.com>
2021-12-27 17:43:31 +0900

    12/27 (월) kg 버튼 클릭시 페이지 이동, 글자 효과

--- Kg/kg.js
+++ Kg/kg.js
@@ -7,21 +7,59 @@ $(document).ready(function(){
         $('.logo2').hide();
     });
 
-    // $('.post-slider').slick();
     $('.s_slick_slider').slick({
         dots: true,
         infinite: true,
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        // rtl: true
-        //오토 플레이 실행 
-      });
-      
-      $('button').click(function(){
-         $('html,body').animate({scrollTop:0},200);
+        autoplay: true,
+        autoplaySpeed:5000,
+        arrows:false,
+        infinite:true
       });
 
-      
-      
+    //   var chkclick= false;
+    //   var Slider = 0;
+
+    //   if (Slide == 0 && chkclick == false)  {
+    //         $('.s_slick_slider').slick('slickPause');
+    //         $('#theVideo').get(0).play();
+
+    //         document.getElementById('theVideo').addEventListener('ended', myHandler, false);
+    //         function myHandler(e) {
+    //             $('.s_slick_slider').slick('slickPlay');
+    //         }
+    //     } if (Slide != 0 && chkclick == false) {
+    //         $('.s_slick_slider').slick('slickPlay');
+    //     }
+
+      $(".slick-dots").append('<button class="stop_btn"></button>');
+      $(".slick-dots").append('<button class="start_btn" style="display:none"></button>');
+
+      $('.stop_btn').click(function(){
+          $('.s_slick_slider').slick('slickPause');
+          $('.stop_btn').css({display: 'none'});
+          $('.start_btn').css({display:'inline-block'});
+      });
+
+      $('.start_btn').click(function(){
+          $('.s_slick_slider').slick('slickPlay');
+          $('.stop_btn').css({display:'inline-block'});
+          $('.start_btn').css({display:'none'});
+      });
+
+      $(".diy_slider_wrap").touchSlider({
+		view : 4,
+
+		btn_prev : $(".conArea").find(".btn_prev1"),
+		btn_next : $(".conArea").find(".btn_next1")
+	});
+    
+    // $('.scroll_a').click(function(){
+    //     var $offset = $('.section2').offset().top;
+    //     $('html,body').animate({    
+    //         scrollTop: $offset
+    //         },500)
+    //     })
 });
